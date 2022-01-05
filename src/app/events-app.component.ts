2e187a1f80fc7e8f9d92a367a6507f851fa962d7
Ben <dartaryan@gmail.com>
2022-01-05 09:58:24 +0200

    Accessing Static Files

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h2>Hello world</h2>',
+  template: `<h2>Hello world</h2>
+             <img src="/assets/images/basic-shield.png/">`
 })
 export class AppComponent {
   title = 'ng-fundamentals';
